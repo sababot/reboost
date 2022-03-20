d53655dbbce48ee39d265e75bc7d3d1360ba6b1f
sababot <twinsgardos@gmail.com>
2022-03-20 23:44:50 +0100

    register

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ mysql = MySQL(app)
 @app.route('/login/', methods=['GET', 'POST'])
 def login():
     # Output message if something goes wrong...
-    msg = 'asdfsadf'
+    msg = ''
 
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
     	# Create variables for easy access
@@ -44,7 +44,42 @@ def login():
             # Account doesnt exist or username/password incorrect
             msg = 'Incorrect username/password!'
 
-    return render_template('index.html', msg='')
+    return render_template('login.html', msg='')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    # Output message if something goes wrong...
+    msg = ''
+    # Check if "username", "password" and "email" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        # Create variables for easy access
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+
+        # Check if account exists using MySQL
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = %s', (username,))
+        account = cursor.fetchone()
+        # If account exists show error and validation checks
+        if account:
+            msg = 'Account already exists!'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address!'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers!'
+        elif not username or not password or not email:
+            msg = 'Please fill out the form!'
+        else:
+            # Account doesnt exists and the form data is valid, now insert new account into accounts table
+            cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s)', (username, password, email,))
+            mysql.connection.commit()
+            msg = 'You have successfully registered!'
+    elif request.method == 'POST':
+        # Form is empty... (no POST data)
+        msg = 'Please fill out the form!'
+    # Show registration form with message (if any)
+    return render_template('register.html', msg=msg)
 
 @app.route('/login/logout')
 def logout():
@@ -57,19 +92,39 @@ def logout():
 
 @app.route('/')
 def home():
-   return render_template('home.html')
+    # Check if user is loggedin
+    if 'loggedin' in session:
+        # User is loggedin show them the home page
+        return render_template('home.html', username=session['username'])
+    # User is not loggedin redirect to login page
+    return redirect(url_for('login'))
 
 @app.route('/habit-tracker')
 def habit_tracker():
-   return render_template('habit-tracker.html')
+    # Check if user is loggedin
+    if 'loggedin' in session:
+        # User is loggedin show them the home page
+        return render_template('habit_tracker.html', username=session['username'])
+    # User is not loggedin redirect to login page
+    return redirect(url_for('login'))
 
 @app.route('/gratitude-journal')
 def gratitude_journal():
-   return render_template('gratitude-journal.html')
+    # Check if user is loggedin
+    if 'loggedin' in session:
+        # User is loggedin show them the home page
+        return render_template('gratitude_journal.html', username=session['username'])
+    # User is not loggedin redirect to login page
+    return redirect(url_for('login'))
 
 @app.route('/time-table')
 def time_table():
-   return render_template('time-table.html')
+    # Check if user is loggedin
+    if 'loggedin' in session:
+        # User is loggedin show them the home page
+        return render_template('time-table.html', username=session['username'])
+    # User is not loggedin redirect to login page
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run()
